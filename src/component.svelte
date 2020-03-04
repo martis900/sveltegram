a3f276014805e7409d7b16659ac467290206ca35
martis900 <martynas.alekna10@gmail.com>
2020-03-04 21:36:17 +0200

    width animation fix

--- src/component.svelte
+++ src/component.svelte
@@ -6,16 +6,22 @@
 <style>
   .container {
     height: 100vh;
-    overflow: scroll;
     display: flex;
     align-items: center;
     flex-direction: column;
     transition: all 2s ease;
   }
+  .main {
+    width: 80vw;
+    overflow: scroll;
+  }
 </style>
 
-<div class="container">
-  {#each $posts as post}
-    <Counter {...post} />
-  {/each}
+<div class="main">
+  <div class="container">
+    {#each $posts as post}
+      <Counter {...post} />
+    {/each}
+  </div>
+
 </div>
